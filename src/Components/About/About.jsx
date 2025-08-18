1da960e6bd878e161845e62ba6ea27bcea84b065
Khushi Rana <khushithakur8725@gmail.com>
2025-08-18 13:19:22 +0530

    Created  about section in jsx file

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/about_profile.svg'
+
 const About = () => {
   return (
-    <div>
-      
+    <div className="about">
+      <div className="about-title">
+        <h1>About Me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>I am experienced Frontend Developer with the strong background of React.js and other frontend technologies such as HTML, CSS, JavaScript, Express</p>
+            <p>My passion for frontend not only for the job and getting placement. </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>React Js</p>
+              <hr style={{width:"80%"}}/>
+            </div>
+
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{width:"80%"}}/>
+            </div>
+
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{width:"70%"}}/>
+            </div>
+
+            <div className="about-skill">
+              <p>Next Js</p>
+              <hr style={{width:"50%"}}/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+ Projects Completed</h1>
+          <p>Tech stack: ReactJs, JavaScript, HTML, CSS, NextJs, Node.js</p>
+        </div>
+        <div className="about-achievement">
+          <h1>Selected for Infosys Springboard Artificial Intelligence Program</h1>
+          <p>Pragati: Path to Future cohort 3</p>
+        </div>
+        <div className="about-achievement">
+          <h1>180+ Leetcode Question Solved</h1>
+          <p>Solved Leetcode Question</p>
+        </div>
+        <div className="about-achievement">
+          <h1>Patent Published</h1>
+          <p>Agri Farmanet(KAVYA FARMNET)</p>
+        </div>
+      </div>
     </div>
   )
 }
