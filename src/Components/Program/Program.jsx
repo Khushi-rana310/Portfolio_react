55e41246feb36f34f0f0d95ff046ce5855e958df
Khushi Rana <khushithakur8725@gmail.com>
2025-08-24 03:15:28 +0530

    updation in all files for responsiveness

--- src/Components/Program/Program.jsx
+++ src/Components/Program/Program.jsx
@@ -5,33 +5,27 @@ const Program = () => {
   return (
     <div id='program' className="program">
         <div className="certification">
-            <h2>Certification / Program</h2>
-            <img src="" alt="" />
+            <h1>Certification / Programs</h1>
+            <div className="about-achievements">
+                <div className="about-achievement">
+                    <h1>10+ Projects Completed</h1>
+                    <p>Tech stack: ReactJs, JavaScript, HTML, CSS, NextJs, Node.js</p>
+                </div>
+                <div className="about-achievement">
+                    <h1>Selected for Infosys Springboard AI Program</h1>
+                    <p>Pragati: Path to Future cohort 3</p>
+               </div>
+               <div className="about-achievement">
+                   <h1>180+ Leetcode Question Solved</h1>
+                   <p>Solved Leetcode Question</p>
+                </div>
+                <div className="about-achievement">
+                    <h1>Patent Published</h1>
+                    <p>Agri Farmanet(KAVYA FARMNET)</p>
+                </div>
+            </div>
         </div>
-        <div className="programs">
-            <img src="" alt="" />
-            <p></p>
-        </div>
-        <div className="programs">
-            <img src="" alt="" />
-            <p></p>
-        </div>
-        <div className="programs">
-            <img src="" alt="" />
-            <p></p>
-        </div>
-        <div className="programs">
-            <img src="" alt="" />
-            <p></p>
-        </div>
-        <div className="programs">
-            <img src="" alt="" />
-            <p></p>
-        </div>
-        <div className="programs">
-            <img src="" alt="" />
-            <p></p>
-        </div>
+        
     </div>
   )
 }
